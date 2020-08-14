f37d80ab56364a8377e9dce2eaaa22bf41da969f
Muhammadfarazkhan123 <farazrafiq64@gmail.com>
2020-08-14 17:23:44 +0500

    procfile

--- app.js
+++ app.js
@@ -29,6 +29,12 @@ app.use((req, res, next) => {
 	error.status = 404;
 	next(error);
 });
+// app.use((req, res, next) => {
+//     res.status(200).json({
+// 		message:"check it"
+// 	})
+// });
+
 
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
