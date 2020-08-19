f779888f46dd4e441ca985db09705b7dd43bcf45
Muhammadfarazkhan123 <farazrafiq64@gmail.com>
2020-08-19 11:31:25 +0500

    change route

--- app.js
+++ app.js
@@ -11,24 +11,24 @@ app.use(BodyParser.urlencoded({ extended: false }))
 app.use(BodyParser.json())
 
 // CORS=>Cross Origin Resource Sharing
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers',
-		'Orgin,X-Requested-With,Content-Type,Accept,Authorization')
+// app.use((req, res, next) => {
+// 	res.header('Access-Control-Allow-Origin', '*');
+// 	res.header('Access-Control-Allow-Headers',
+// 		'Orgin,X-Requested-With,Content-Type,Accept,Authorization')
 
-	if (req.method === 'OPTIONS') {
-		res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET')
-		return res.status(200).json({})
-	}
-	next()
-})
+// 	if (req.method === 'OPTIONS') {
+// 		res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET')
+// 		return res.status(200).json({})
+// 	}
+// 	next()
+// })
 
 app.use('/',TodoList)
-app.use((req, res, next) => {
-    const error = new Error('not found');
-	error.status = 404;
-	next(error);
-});
+// app.use((req, res, next) => {
+//     const error = new Error('not found');
+// 	error.status = 404;
+// 	next(error);
+// });
 // app.use((req, res, next) => {
 //     res.status(200).json({
 // 		message:"check it"
@@ -36,13 +36,13 @@ app.use((req, res, next) => {
 // });
 
 
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-	res.json({
-        error: {
-            message: error.message,
-		},
-	});
-});
+// app.use((error, req, res, next) => {
+//     res.status(error.status || 500);
+// 	res.json({
+//         error: {
+//             message: error.message,
+// 		},
+// 	});
+// });
 
 module.exports = app;
